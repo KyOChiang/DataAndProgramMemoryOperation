c2aa2fcdf8a304318a540c45e4bb809f609efa33
KyO <yong931231@hotmail.com>
2014-04-07 18:06:56 +0800

    Add test for tbldr+* to test validify of op1, op2, op3 and table pointer, should throw exception if found invalid.

--- test/test_tblrdPreInc.c
+++ test/test_tblrdPreInc.c
@@ -16,6 +16,111 @@
 void setUp(){}
 void tearDown(){}
 
+void test_tblrdPreInc_should_not_read_data_from_TABLE_if_table_ptr_is_invalid(){
+	CEXCEPTION_T ERR;
+
+	//Test fixture
+	Bytecode code = {.instruction = {.mnemonic = TBLRD_PREINC, .name = "tblrd_PreInc"},
+					 .operand1 = -1,
+					 .operand2 = -1,
+					 .operand3 = -1,
+					 .absoluteAddress = 0xD0
+					};
+					
+	FSR[TABLAT] = 0x01;
+	FSR[TBLPTRU] = 0x3A;
+	FSR[TBLPTRH] = 0x10;
+	FSR[TBLPTRL] = 0x05;
+	TABLE[0x000006] = 0x95;
+
+	Try{
+		tblrd_PreInc(&code);
+	} Catch(ERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_TBL_PTR,ERR);
+	}
+
+	TEST_ASSERT_EQUAL(0xD0, code.absoluteAddress);
+}
+
+void test_tblrdPreInc_should_throw_invalid_if_operand1_is_non_empty(){
+	CEXCEPTION_T ERR;
+
+	//Test fixture
+	Bytecode code = {.instruction = {.mnemonic = TBLRD_PREINC, .name = "tblrd_PreInc"},
+					 .operand1 = 0x01,
+					 .operand2 = -1,
+					 .operand3 = -1,
+					 .absoluteAddress = 0xE0
+					};
+					
+	FSR[TABLAT] = 0x01;
+	FSR[TBLPTRU] = 0x00;
+	FSR[TBLPTRH] = 0x10;
+	FSR[TBLPTRL] = 0x05;
+	TABLE[0x000006] = 0x95;
+
+	Try{
+		tblrd_PreInc(&code);
+	} Catch(ERR){
+		TEST_ASSERT_EQUAL(ERR_NON_EMPTY_OPERAND,ERR);
+	}
+
+	TEST_ASSERT_EQUAL(0xE0, code.absoluteAddress);
+}
+
+void test_tblrdPreInc_should_throw_invalid_if_operand2_is_non_empty(){
+	CEXCEPTION_T ERR;
+
+	//Test fixture
+	Bytecode code = {.instruction = {.mnemonic = TBLRD_PREINC, .name = "tblrd_PreInc"},
+					 .operand1 = -1,
+					 .operand2 = 0x01,
+					 .operand3 = -1,
+					 .absoluteAddress = 0xF1
+					};
+					
+	FSR[TABLAT] = 0x01;
+	FSR[TBLPTRU] = 0x00;
+	FSR[TBLPTRH] = 0x10;
+	FSR[TBLPTRL] = 0x05;
+	TABLE[0x000006] = 0xF2;
+
+	Try{
+		tblrd_PreInc(&code);
+	} Catch(ERR){
+		TEST_ASSERT_EQUAL(ERR_NON_EMPTY_OPERAND,ERR);
+	}
+
+	TEST_ASSERT_EQUAL(0xF1, code.absoluteAddress);
+}
+
+void test_tblrdPreInc_should_throw_invalid_if_operand3_is_non_empty(){
+	CEXCEPTION_T ERR;
+
+	//Test fixture
+	Bytecode code = {.instruction = {.mnemonic = TBLRD_PREINC, .name = "tblrd_PreInc"},
+					 .operand1 = -1,
+					 .operand2 = 0x01,
+					 .operand3 = BANKED,
+					 .absoluteAddress = 0xF1
+					};
+					
+	FSR[TABLAT] = 0xA1;
+	FSR[TBLPTRU] = 0x00;
+	FSR[TBLPTRH] = 0x10;
+	FSR[TBLPTRL] = 0x05;
+	TABLE[0x000006] = 0xF2;
+
+	Try{
+		tblrd_PreInc(&code);
+	} Catch(ERR){
+		TEST_ASSERT_EQUAL(ERR_NON_EMPTY_OPERAND,ERR);
+	}
+
+	TEST_ASSERT_EQUAL(0xF1, code.absoluteAddress);
+}
+
+
 void test_tblrdPreInc_should_read_data_from_TABLE_addr_0x000006_and_store_in_TABLAT_after_tblptr_increment_by_1(){
 	CEXCEPTION_T operandERR;
 
